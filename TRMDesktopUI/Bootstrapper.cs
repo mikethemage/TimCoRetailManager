c37b8aac1296ed2b49c4cb18c4dcec8d69dd9167
Mike Dunn <44652427+mikethemage@users.noreply.github.com>
2023-10-09 14:40:13 +0100

    Wire Up Login Form to API
    Connects the login form button to the authentication API endpoint (/token).  Gets back the bearer token or an exception if failed.

--- TRMDesktopUI/Bootstrapper.cs
+++ TRMDesktopUI/Bootstrapper.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using TRMDesktopUI.Helpers;
 using TRMDesktopUI.ViewModels;
 
 namespace TRMDesktopUI
@@ -13,9 +15,14 @@ namespace TRMDesktopUI
     {
         private SimpleContainer _container = new SimpleContainer();
 
-        public Bootstrapper() 
+        public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
         }
 
         protected override void Configure()
@@ -24,7 +31,8 @@ namespace TRMDesktopUI
 
             _container
                 .Singleton<IWindowManager, WindowManager>()
-                .Singleton<IEventAggregator, EventAggregator>();
+                .Singleton<IEventAggregator, EventAggregator>()
+                .Singleton<IAPIHelper, APIHelper>();
 
             GetType().Assembly.GetTypes()
                 .Where(type => type.IsClass)
---
+++ TRMDesktopUI/Models/AuthenticatedUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDesktopUI.Models
+{    
+    public class AuthenticatedUser
+    {
+        public string Access_Token { get; set; }
+        public string UserName { get; set; }
+
+
+        //Do we need these?:
+        public string token_type { get; set; }
+        public int expires_in { get; set; }        
+        public string issued { get; set; }
+        public string expires { get; set; }
+    }
+}
