df37e74b16a25e7581d2205118286fd92e46ff75
Mike Dunn <44652427+mikethemage@users.noreply.github.com>
2023-10-10 19:15:06 +0100

    Update Cart Quantities
    Added automapper and INotifyPropertyChanged to enable cart quantity changes to be reflected.

--- TRMDesktopUI.Library/Models/CartItemModel.cs
+++ TRMDesktopUI.Library/Models/CartItemModel.cs
@@ -9,13 +9,6 @@ namespace TRMDesktopUI.Library.Models
     public class CartItemModel
     {
         public ProductModel Product { get; set; }
-        public int QuantityInCart { get; set; }
-        public string DisplayText
-        {
-            get
-            {
-                return $"{Product.ProductName} ({QuantityInCart})";
-            }
-        }
+        public int QuantityInCart { get; set; }        
     }
 }
