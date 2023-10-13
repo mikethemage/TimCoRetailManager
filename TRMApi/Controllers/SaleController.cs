3f1f8fb9e48627e0c227488c065a2efd384292eb
Mike Dunn <44652427+mikethemage@users.noreply.github.com>
2023-10-13 22:30:57 +0100

    Add proper connectionstring lookup

--- TRMApi/Controllers/SaleController.cs
+++ TRMApi/Controllers/SaleController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Security.Claims;
 using TRMDataManager.Library.DataAccess;
@@ -13,10 +14,17 @@ namespace TRMApi.Controllers
     [ApiController]
     public class SaleController : ControllerBase
     {
+        private readonly IConfiguration _config;
+
+        public SaleController(IConfiguration config)
+        {
+            _config = config;
+        }
+
         [Authorize(Roles = "Cashier")]
         public void Post(SaleModel sale)
         {
-            SaleData data = new SaleData();
+            SaleData data = new SaleData(_config);
             string userId = User.FindFirstValue(ClaimTypes.NameIdentifier); //RequestContext.Principal.Identity.GetUserId();
             data.SaveSale(sale, userId);
         }
@@ -25,7 +33,7 @@ namespace TRMApi.Controllers
         [Route("GetSalesReport")]
         public List<SaleReportModel> GetSalesReport()
         {
-            SaleData data = new SaleData();
+            SaleData data = new SaleData(_config);
             return data.GetSaleReport();
         }
     }
--- TRMApi/Program.cs
+++ TRMApi/Program.cs
@@ -17,10 +17,11 @@ namespace TRMApi
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            Host.CreateDefaultBuilder(args)            
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    
                 });
     }
 }
