8190b3b11332a1fffc6a07a2b038811de4efe5ca
Mike Dunn <mdunn147@gmail.com>
2023-10-13 09:45:15 +0100

    UserDisplayModel code formatting

--- TRMDesktopUI/ViewModels/UserDisplayViewModel.cs
+++ TRMDesktopUI/ViewModels/UserDisplayViewModel.cs
@@ -1,10 +1,8 @@
 ﻿using Caliburn.Micro;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Dynamic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using TRMDesktopUI.Library.Api;
