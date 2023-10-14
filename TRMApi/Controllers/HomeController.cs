27bb399ae836a552cab696375b014c32ceceff28
Mike Dunn <44652427+mikethemage@users.noreply.github.com>
2023-10-14 20:00:22 +0100

    Temp re-enable privacy page to reseed user data

--- TRMApi/Controllers/HomeController.cs
+++ TRMApi/Controllers/HomeController.cs
@@ -30,25 +30,25 @@ namespace TRMApi.Controllers
 
         public async Task<IActionResult> Privacy()
         {
-            //string[] roles = { "Admin", "Manager", "Cashier" };
+            string[] roles = { "Admin", "Manager", "Cashier" };
 
-            //foreach (var role in roles)
-            //{
-            //    var roleExist = await _roleManager.RoleExistsAsync(role);
+            foreach (var role in roles)
+            {
+                var roleExist = await _roleManager.RoleExistsAsync(role);
 
-            //    if (roleExist == false)
-            //    {
-            //        await _roleManager.CreateAsync(new IdentityRole(role));
-            //    }
-            //}
+                if (roleExist == false)
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
 
-            //var user = await _userManager.FindByEmailAsync("tim@iamtimcorey.com");
+            var user = await _userManager.FindByEmailAsync("tim@iamtimcorey.com");
 
-            //if(user != null)
-            //{
-            //    await _userManager.AddToRoleAsync(user, "Admin");
-            //    await _userManager.AddToRoleAsync(user, "Cashier");
-            //}
+            if (user != null)
+            {
+                await _userManager.AddToRoleAsync(user, "Admin");
+                await _userManager.AddToRoleAsync(user, "Cashier");
+            }
 
             return View();
         }
